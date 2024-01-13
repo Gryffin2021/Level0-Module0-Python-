d74ab152a40f04786b93076e7fcaba5405f39e31
league <gryffinwilson@gmail.com>
2024-01-12 18:23:17 -0800

    greeter, shapes and colors, turtle spiral, pentagon crazy, tashe me, flaming ninja star, logical turtle

--- _01_drawing/_c_logical_turtle.py
+++ _01_drawing/_c_logical_turtle.py
@@ -13,20 +13,21 @@ def screen_clicked(x, y):
     print('You pressed: x=' + str(x) + ', y=' + str(y))
     
     # 6. Call the turtle .penup() method
-    
+    timmy.penup()
     # 7. Move the turtle to a new location using .goto(x, y)
-
+    timmy.goto(200, 0)
 
 def turtle_clicked(x, y):
     print('turtle clicked!')
     
     # 8. Make a for loop to run the next instructions 3 times
-        
+    for i in range(3):
         # 9. Make the turtle spin 360 degrees using the .right() method
-        
+        timmy.right(360)
         # 10. Use the .color() method and getRandomColor() function to change
         # the color of the turtle,
         # myTurtle.color(get_random_color())
+        timmy.color(get_random_color())
 
 
 if __name__ == '__main__':
@@ -34,16 +35,17 @@ if __name__ == '__main__':
     window.setup(width=0.75, height=0.8, startx=0, starty=0)
     
     # 1. Make a new turtle
-    
+    timmy = turtle.Turtle()
     # 2. Make your turtle's shape 'turtle', .shape('turtle')
-    
+    timmy.shape('turtle')
     # 3. Set your turtle's color using .color('green') and .pencolor('blue')
-    
+    timmy.color('green')
+    timmy.pencolor('blue')
     # 4. Set and new width, length, and outline of our turtle
     #    my_turtle.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
-
+    timmy.turtlesize(stretch_wid=10, stretch_len=10, outline=4)
     # 5. Uncomment the following line and replace 'my_turtle' with your turtle
-    # my_turtle.onclick(turtle_clicked)
+    timmy.onclick(turtle_clicked)
 
 # ===================== DO NOT EDIT THE CODE BELOW ============================
     window.onclick(screen_clicked)
--- _02_code_flow/_a_turtle_spiral.py
+++ _02_code_flow/_a_turtle_spiral.py
@@ -14,23 +14,23 @@ if __name__ == '__main__':
     window.bgcolor('white')
     
     # Make a new turtle
-    
+    timmy = turtle.Turtle()
     # This code sets our shape to a turtle
-    
+    timmy.shape('turtle')
     # Set your turtle's speed (0=fastest, 1=slowest, 10=faster)
-    
+    timmy.speed(0)
     # Set your turtle's color using .color('green')
-    
+    timmy.color('green')
     # Use a loop to repeat a the code below 50 times
-        
+    for i in range(50):
         # Set the turtle color to a random color
-
+        timmy.color(get_random_color())
         # Move the turtle (5*i) pixels. 'i' is the loop variable
-        
+        timmy.forward(5*i)
         # Turn the turtle (360/7) degrees to the right
-         
+        timmy.right(360/7)
         # Change the turtle width to 'i' (the loop variable)
-        
+        timmy.width(i)
         # Check the pattern against the picture in the recipe. If it matches, you are done!
     
 # ===================== DO NOT EDIT THE CODE BELOW ============================
